73b7d219b86ff09bdc2dca8bd3e36541f4826be9
Heidrun Kubiessa <heidrun.kubiessa@utah.edu>
2023-10-11 11:30:08 -0600

    connected api to front end and tebale to AWS cloud

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const userRoutes = require('./routes/user-routes');
+require('dotenv').config();
 
 // express middleware, used to be bodyparser
 app.use(express.json());
